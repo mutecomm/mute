3def3af6924f03bb7df029dcf8d2571139bce18c
Frank Braun <frank@cryptogroup.net>
2015-09-25 06:28:13 +0200

    add mutecrypt command

---
+++ cmd/mutecrypt/mutecrypt.go
@@ -0,0 +1,49 @@
+// mutecrypt is the crypt tool for Mute which handles message encryption, message decryption and key management.
+package main
+
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/mutecomm/mute/cryptengine"
+	"github.com/mutecomm/mute/log"
+	"github.com/mutecomm/mute/release"
+	"github.com/mutecomm/mute/util"
+	"github.com/mutecomm/mute/util/interrupt"
+)
+
+func init() {
+	cli.VersionPrinter = release.PrintVersion
+}
+
+func mutecryptMain() error {
+	defer log.Flush()
+
+	// create crypto engine
+	ce := cryptengine.New()
+	defer ce.Close()
+
+	// add interrupt handler
+	interrupt.AddInterruptHandler(func() {
+		log.Infof("gracefully shutting down...")
+		ce.Close()
+	})
+
+	// start crypto engine
+	go func() {
+		if err := ce.Start(os.Args); err != nil {
+			interrupt.ShutdownChannel <- err
+			return
+		}
+		interrupt.ShutdownChannel <- nil
+	}()
+
+	return <-interrupt.ShutdownChannel
+}
+
+func main() {
+	// work around defer not working after os.Exit()
+	if err := mutecryptMain(); err != nil {
+		util.Fatal(err)
+	}
+}
