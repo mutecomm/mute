1bb95bfcc5cc37aba2132e360a6cb1f20ca458f8
Frank Braun <frank@cryptogroup.net>
2015-09-25 04:15:24 +0200

    add times package

---
+++ util/times/times.go
@@ -0,0 +1,36 @@
+// Package times contains time utility functions for mute.
+package times
+
+import (
+	"time"
+)
+
+// Day defines the number of seconds in a day.
+const Day = uint64(24 * 60 * 60)
+
+// Now returns the current time in UTC as Unix time,
+// the number of seconds elapsed since January 1, 1970 UTC.
+func Now() int64 {
+	return time.Now().UTC().Unix()
+}
+
+// NowNano returns the current time in UTC as Unix time,
+// the number of nanoseconds elapsed since January 1, 1970 UTC.
+func NowNano() int64 {
+	return time.Now().UTC().UnixNano()
+}
+
+// OneYearLater returns the time one year later from now in UTC.
+func OneYearLater() int64 {
+	return time.Now().UTC().AddDate(1, 0, 0).Unix()
+}
+
+// ThirtyDaysLater returns the time 30 days later from now in UTC.
+func ThirtyDaysLater() int64 {
+	return time.Now().UTC().AddDate(0, 0, 30).Unix()
+}
+
+// NinetyDaysLater returns the time 90 days later from now in UTC.
+func NinetyDaysLater() int64 {
+	return time.Now().UTC().AddDate(0, 0, 90).Unix()
+}
