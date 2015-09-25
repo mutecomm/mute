9bb7724e52aa3033bf73586ccd4a9fa1ff056757
Frank Braun <frank@cryptogroup.net>
2015-09-25 05:51:14 +0200

    add muteproto command

---
+++ cmd/muteproto/muteproto.go
@@ -0,0 +1,38 @@
+// muteproto implements the Mute message protocol for sending and receiving
+// encrypted messages.
+package main
+
+import (
+	"os"
+
+	"github.com/mutecomm/mute/log"
+	"github.com/mutecomm/mute/protoengine"
+	"github.com/mutecomm/mute/release"
+	"github.com/mutecomm/mute/util"
+
+	"github.com/codegangsta/cli"
+)
+
+func init() {
+	cli.VersionPrinter = release.PrintVersion
+}
+
+func muteprotoMain() error {
+	defer log.Flush()
+
+	// create proto engine
+	pe := protoengine.New()
+
+	// run proto engine
+	if err := pe.Run(os.Args); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	// work around defer not working after os.Exit()
+	if err := muteprotoMain(); err != nil {
+		util.Fatal(err)
+	}
+}
