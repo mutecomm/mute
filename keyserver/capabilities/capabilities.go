260033d4d01fa6c9b5dd7d19b2f8d559e6883bbc
Frank Braun <frank@cryptogroup.net>
2015-09-26 18:12:38 +0200

    remove unnecessary code

--- keyserver/capabilities/capabilities.go
+++ keyserver/capabilities/capabilities.go
@@ -1,22 +1,6 @@
 // Package capabilities defines the capabilities of the Mute key server.
 package capabilities
 
-var (
-	// DefaultMethods defines the implemented methods of the key server.
-	DefaultMethods = []string{
-		"KeyRepository.Capabilities",
-		"KeyRepository.CreateUID",
-		"KeyRepository.FetchUID",
-		"KeyRepository.UpdateUID",
-		"KeyHashchain.FetchHashChain",
-		"KeyHashchain.FetchLastHashChain",
-		"KeyHashchain.LookupUID",
-		"KeyInitRepository.AddKeyInit",
-		"KeyInitRepository.FetchKeyInit",
-		"KeyInitRepository.FlushKeyInit",
-	}
-)
-
 // The Capabilities of a Mute key server.
 type Capabilities struct {
 	METHODS               []string // methods implemented from specification
