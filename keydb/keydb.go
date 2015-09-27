106950f70ebe7cfeadebb1f04306f8c5d84025ef
Frank Braun <frank@cryptogroup.net>
2015-09-27 04:42:59 +0200

    improve documentation

--- keydb/keydb.go
+++ keydb/keydb.go
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package keydb defines an encrypted database used to store keys.
+// Package keydb defines an encrypted database used to store cryptographic
+// keys.
 package keydb
 
 import (
