e0e823d2a496d9cbe8209c8016b3aff89a8f5795
Frank Braun <frank@cryptogroup.net>
2015-09-28 20:07:34 +0200

    improve documentation

--- mix/smtpclient/client.go
+++ mix/smtpclient/client.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package smtpclient wraps pkg/smtp to make it easy to use
+// Package smtpclient wraps pkg/smtp to make it easy to use.
 package smtpclient
 
 import (
--- serviceguard/client/packetproto/client.go
+++ serviceguard/client/packetproto/client.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package packetproto implements a client of an issuer
+// Package packetproto implements a client of an issuer.
 package packetproto
 
 import (
