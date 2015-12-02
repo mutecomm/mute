6d1c0a4ece55d65a9408ee6f2c47c7b7df57805c
Frank Braun <frank@cryptogroup.net>
2015-12-02 00:11:18 +0000

    padding: improve test

--- msg/padding/padding_test.go
+++ msg/padding/padding_test.go
@@ -8,14 +8,18 @@ import (
 	"testing"
 
 	"github.com/mutecomm/mute/cipher"
+	"github.com/mutecomm/mute/encode/base64"
 )
 
 func TestGenerate(t *testing.T) {
-	if _, err := Generate(100, cipher.RandFail); err == nil {
+	if _, err := Generate(23, cipher.RandFail); err == nil {
 		t.Error("should fail")
 	}
-	_, err := Generate(100, cipher.RandReader)
+	padding, err := Generate(23, cipher.RandZero)
 	if err != nil {
 		t.Fatal(err)
+	}
+	if base64.Encode(padding) != "3JXAeKJAiYmtSKIUkoQgh1MPivvHRTY=" {
+		t.Error("wrong padding")
 	}
 }
