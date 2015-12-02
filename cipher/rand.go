ef24946bff1affebf50d0d46b0f8481976494d94
Frank Braun <frank@cryptogroup.net>
2015-12-02 00:05:36 +0000

    cipher: add RandZero

--- cipher/rand.go
+++ cipher/rand.go
@@ -22,3 +22,15 @@ type eofReader struct{}
 func (e eofReader) Read(p []byte) (n int, err error) {
 	return 0, io.EOF
 }
+
+// RandZero is a Reader that always returns 0.
+var RandZero = zeroReader{}
+
+type zeroReader struct{}
+
+func (z zeroReader) Read(p []byte) (n int, err error) {
+	for i := 0; i < len(p); i++ {
+		p[i] = 0
+	}
+	return len(p), nil
+}
--- cipher/rand_test.go
+++ cipher/rand_test.go
@@ -14,3 +14,19 @@ func TestRandFail(t *testing.T) {
 		t.Fatal("should fail")
 	}
 }
+
+func TestRandZero(t *testing.T) {
+	b := make([]byte, 32)
+	n, err := RandZero.Read(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != 32 {
+		t.Error("n != 32")
+	}
+	for i := 0; i < len(b); i++ {
+		if b[i] != 0 {
+			t.Errorf("b[%d] != 0", i)
+		}
+	}
+}
