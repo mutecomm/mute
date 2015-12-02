a3e7e6fc73cd17cfbc0ac62524ec68b0889fe412
Frank Braun <frank@cryptogroup.net>
2015-12-02 03:09:49 +0000

    length: define maximum mix and nym address lengths

--- uid/length/length.go
+++ uid/length/length.go
@@ -12,3 +12,11 @@ const KeyEntryECDHE25519 = 247
 
 // Nil defines the length of a JSON encoded nil value.
 const Nil = 4
+
+// MaxMixAddress defines the maximum length of a mix address.
+// The restriction is the same as with email addresses.
+const MaxMixAddress = 254
+
+// MaxNymAddress defines the maximum length of a base64 encoded nym address.
+// The maximum length of a unencoded nym address is 1176. 4*(1176/3) = 1568.
+const MaxNymAddress = 1568
