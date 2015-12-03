599241e052397da2c769026c39703af654fbecb1
Frank Braun <frank@cryptogroup.net>
2015-12-03 05:09:19 +0000

    hashchain: define length constants

--- keyserver/hashchain/hashchain.go
+++ keyserver/hashchain/hashchain.go
@@ -18,6 +18,12 @@ const TestEntry = "PxIx7lxwcKB3vmPLGzqm3alBCBkHbD89qRBWs7+N8yMB6QEQSe7yf4BrMISdY
 // Type denotes the current hash chain type.
 var Type = []byte{0x01}
 
+// EntryBase64Len define the length of a hashchain entry in base64 form
+const EntryBase64Len = 204
+
+// EntryByteLen defines the length of a hashchain entry in byte form.
+const EntryByteLen = 153
+
 // SplitEntry splits a base64 encoded key hashchain entry. Specification:
 // https://github.com/mutecomm/mute/blob/master/doc/keyserver.md#key-hashchain-operation
 func SplitEntry(entry string) (hash, typ, nonce, hashID, crUID, uidIndex []byte, err error) {
@@ -25,9 +31,10 @@ func SplitEntry(entry string) (hash, typ, nonce, hashID, crUID, uidIndex []byte,
 	if err != nil {
 		return nil, nil, nil, nil, nil, nil, log.Error(err)
 	}
-	if len(e) != 153 {
+	if len(e) != EntryByteLen {
 		return nil, nil, nil, nil, nil, nil,
-			log.Errorf("hashchain: entry '%s' does not have byte length 153 (but %d)", entry, len(e))
+			log.Errorf("hashchain: entry '%s' does not have byte length %d (but %d)",
+				entry, EntryByteLen, len(e))
 	}
 	// HASH(entry[n]) | TYPE | NONCE | HashID | CrUID | UIDIndex
 	hash = e[:32]
