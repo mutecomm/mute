100813dec7404e56dd235ade081264acea891dcb
Frank Braun <frank@cryptogroup.net>
2015-12-09 06:35:55 +0000

    msg: extend KeyStore test

--- msg/keystore_test.go
+++ msg/keystore_test.go
@@ -96,4 +96,10 @@ func TestKeyStore(t *testing.T) {
 	if res.String() != msgs.Message1 {
 		t.Fatal("messages differ")
 	}
+	// make recipient key has been deleted
+	// TODO: make sure myID/contactID order makes sense everywhere!
+	_, err = bobKeyStore.GetMessageKey(alice, bob, false, 0)
+	if err != msg.ErrMessageKeyUsed {
+		t.Error("should fail with msg.ErrMessageKeyUsed")
+	}
 }
