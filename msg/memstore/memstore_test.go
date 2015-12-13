b37c9d936d2286b508017827603214b7ed911671
Frank Braun <frank@cryptogroup.net>
2015-12-13 07:50:25 +0000

    memstore: more unit tests

--- msg/memstore/memstore_test.go
+++ msg/memstore/memstore_test.go
@@ -5,15 +5,19 @@
 package memstore
 
 import (
+	"bytes"
+	"io"
 	"testing"
 
 	"github.com/mutecomm/mute/cipher"
+	"github.com/mutecomm/mute/encode/base64"
 	"github.com/mutecomm/mute/keyserver/hashchain"
+	"github.com/mutecomm/mute/msg"
 	"github.com/mutecomm/mute/uid"
 	"github.com/mutecomm/mute/util/times"
 )
 
-func TestMemStore(t *testing.T) {
+func TestKeyEntry(t *testing.T) {
 	ms := New()
 	msg, err := uid.Create("alice@mute.berlin", false, "", "", uid.Strict,
 		hashchain.TestEntry, cipher.RandReader)
@@ -40,8 +44,85 @@ func TestMemStore(t *testing.T) {
 	if _, err := ms.FindKeyEntry("MUTE"); err == nil {
 		t.Error("should fail")
 	}
-	err = ms.StoreSession("alice@mute.berlin", "bob@mute.berlin", "", "", nil, nil)
+}
+
+func genMessageKey() (*[64]byte, error) {
+	var messageKey [64]byte
+	if _, err := io.ReadFull(cipher.RandReader, messageKey[:]); err != nil {
+		return nil, err
+	}
+	return &messageKey, nil
+}
+
+func TestSessenStore(t *testing.T) {
+	ms := New()
+	sendKey, err := genMessageKey()
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
+	}
+	recvKey, err := genMessageKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ms.StoreSession("alice@mute.berlin", "bob@mute.berlin",
+		base64.Encode(cipher.SHA512([]byte("rootkey"))),
+		base64.Encode(cipher.SHA512([]byte("chainkey"))),
+		[]string{base64.Encode(sendKey[:])},
+		[]string{base64.Encode(recvKey[:])})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// test sender key
+	key, err := ms.GetMessageKey("alice@mute.berlin", "bob@mute.berlin", true, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(key[:], sendKey[:]) {
+		t.Error("send key differs")
+	}
+	err = ms.DelMessageKey("alice@mute.berlin", "bob@mute.berlin", true, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = ms.GetMessageKey("alice@mute.berlin", "bob@mute.berlin", true, 0)
+	if err != msg.ErrMessageKeyUsed {
+		t.Error("should fail with msg.ErrMessageKeyUsed")
+	}
+	// test receiver key
+	key, err = ms.GetMessageKey("alice@mute.berlin", "bob@mute.berlin", false, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(key[:], recvKey[:]) {
+		t.Error("recv key differs")
+	}
+	err = ms.DelMessageKey("alice@mute.berlin", "bob@mute.berlin", false, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = ms.GetMessageKey("alice@mute.berlin", "bob@mute.berlin", false, 0)
+	if err != msg.ErrMessageKeyUsed {
+		t.Error("should fail with msg.ErrMessageKeyUsed")
+	}
+}
+
+func TestSessionState(t *testing.T) {
+	ms := New()
+	ss := &msg.SessionState{
+		SenderSessionCount:    1,
+		SenderMessageCount:    2,
+		RecipientSessionCount: 3,
+		RecipientMessageCount: 4,
+	}
+	err := ms.SetSessionState("alice@mute.berlin", "bob@mute.berlin", ss)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sss, err := ms.GetSessionState("alice@mute.berlin", "bob@mute.berlin")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ss != sss {
+		t.Error("session states differ")
 	}
 }
