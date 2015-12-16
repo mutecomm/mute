3ec4e88d760e82b84f97dd492b4ab7197ee3a682
Frank Braun <frank@cryptogroup.net>
2015-12-16 18:09:57 +0000

    msg: extend TestGenerateMessageKeys()

--- msg/keys_test.go
+++ msg/keys_test.go
@@ -5,14 +5,17 @@
 package msg
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/mutecomm/mute/cipher"
 	"github.com/mutecomm/mute/encode/base64"
+	"github.com/mutecomm/mute/log"
 	"github.com/mutecomm/mute/msg/session/memstore"
 )
 
 func TestGenerateMessageKeys(t *testing.T) {
+	defer log.Flush()
 	rk, err := base64.Decode("HdxGXLlSNRhwydGkd4QISqquIQirNtCD")
 	if err != nil {
 		t.Fatal(err)
@@ -33,16 +36,17 @@ func TestGenerateMessageKeys(t *testing.T) {
 	copy(recipientPub[:], rp)
 	a := "alice@mute.berlin"
 	b := "bob@mute.berlin"
-	h := base64.Encode(cipher.SHA512(senderSessionPub[:]))
-	ms := memstore.New()
+	h1 := base64.Encode(cipher.SHA512(senderSessionPub[:]))
+	h2 := base64.Encode(cipher.SHA512(recipientPub[:]))
+	ms1 := memstore.New()
 
 	err = generateMessageKeys(a, b, rootKey[:], false, &senderSessionPub,
-		&recipientPub, NumOfFutureKeys, ms)
+		&recipientPub, NumOfFutureKeys, ms1)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rootKeyHash, err := ms.GetRootKeyHash(a, b, h)
+	rootKeyHash, err := ms1.GetRootKeyHash(a, b, h1)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -51,7 +55,7 @@ func TestGenerateMessageKeys(t *testing.T) {
 		t.Error("wrong rootKeyHash")
 	}
 
-	key, err := ms.GetMessageKey(a, b, h, true, 0)
+	key, err := ms1.GetMessageKey(a, b, h1, true, 0)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,7 +64,7 @@ func TestGenerateMessageKeys(t *testing.T) {
 		t.Error("wrong message key (sender, 0)")
 	}
 
-	key, err = ms.GetMessageKey(a, b, h, false, 0)
+	key, err = ms1.GetMessageKey(a, b, h1, false, 0)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -69,7 +73,7 @@ func TestGenerateMessageKeys(t *testing.T) {
 		t.Error("wrong message key (recipient, 0)")
 	}
 
-	key, err = ms.GetMessageKey(a, b, h, true, 49)
+	key, err = ms1.GetMessageKey(a, b, h1, true, 49)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -78,12 +82,68 @@ func TestGenerateMessageKeys(t *testing.T) {
 		t.Error("wrong message key (sender, 49)")
 	}
 
-	key, err = ms.GetMessageKey(a, b, h, false, 49)
+	key, err = ms1.GetMessageKey(a, b, h1, false, 49)
 	if err != nil {
 		t.Fatal(err)
 	}
 	b64 = base64.Encode(key[:])
 	if b64 != "9M98fdyq9z5gb9seYUPcw0Gw1Ec5H3fF0EPVVUdfw7OPnGYfolAJ+UVvgqdzkpklCdx6r2SqyhV3Sbes2QEd6A==" {
 		t.Error("wrong message key (recipient, 49)")
+	}
+
+	// generate additional keys from chainKey
+	chainKey, err := ms1.GetChainKey(a, b, h1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = generateMessageKeys(a, b, chainKey[:], false, &senderSessionPub,
+		&recipientPub, NumOfFutureKeys, ms1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// generate all keys at the same time
+	ms2 := memstore.New()
+	copy(rootKey[:], rk)
+	err = generateMessageKeys(a, b, rootKey[:], true, &senderSessionPub,
+		&recipientPub, 2*NumOfFutureKeys, ms2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	n1, err := ms1.NumMessageKeys(a, b, h1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	n2, err := ms2.NumMessageKeys(b, a, h2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n1 != n2 {
+		t.Error("number of message keys differ")
+	}
+
+	// compare keys
+	k1, err := ms1.GetMessageKey(a, b, h1, true, NumOfFutureKeys-1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	k2, err := ms2.GetMessageKey(b, a, h2, false, NumOfFutureKeys-1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(k1[:], k2[:]) {
+		t.Error("keys differ")
+	}
+
+	k1, err = ms1.GetMessageKey(a, b, h1, true, 2*NumOfFutureKeys-1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	k2, err = ms2.GetMessageKey(b, a, h2, false, 2*NumOfFutureKeys-1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(k1[:], k2[:]) {
+		t.Error("keys differ")
 	}
 }
