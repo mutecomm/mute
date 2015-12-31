aba58cfa26813a4329e3ffbc7292cba3b42aa37e
Jonathan Logan <j.logan@cryptohippie.net>
2015-12-31 12:47:58 +0000

    Mutt Integration

---
+++ contrib/mutt/MuteSend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo $* >> /tmp/sendlog
+username=$(cat ~/.config/mute/MuttUser)
+
+tonym=${2}
+msgfile=$(mktemp)
+cat - > ${msgfile}
+exec 3<~/.config/mute/passphrase.f ; mutectrl contact add --id "${username}" --contact "${tonym}" 2>> /tmp/sendlog >> /tmp/sendlog
+exec 3<~/.config/mute/passphrase.f ; mutectrl msg add --from "${username}" --to "${tonym}" --file ${msgfile} 2>> /tmp/sendlog >> /tmp/sendlog
+echo "mutectrl msg add --from "${username}" --to "${tonym}" --file ${msgfile}" 2>> /tmp/sendlog >> /tmp/sendlog
+rm ${msgfile}
+exec 3<~/.config/mute/passphrase.f ; mutectrl msg send --id "${username}" 2>> /tmp/sendlog >> /tmp/sendlog
+exit 0
