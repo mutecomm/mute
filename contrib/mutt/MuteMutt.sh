0d669fa2b315a6b1cd7ae8f05ba4cb45c90c67f5
Frank Braun <frank@cryptogroup.net>
2016-01-02 16:53:25 +0000

    make sure username was set

--- contrib/mutt/MuteMutt.sh
+++ contrib/mutt/MuteMutt.sh
@@ -3,6 +3,10 @@
 if [ "${1}" != "" ]; then
 	echo "${1}" >  ~/.config/mute/MuttUser
 fi
+if [ ! -f ~/.config/mute/MuttUser ]; then
+  echo "No username set. Call ${0} with username argument." >&2
+  exit 1
+fi
 echo "WARNING: Usage of MuteMutt is insecure since the passphrase is chached on disk."
 read -p "Passphrase: " -s passphrase
 echo
