5eb6969a7e9847a7d75332423a3f18a895c6577e
Jonathan Logan <j.logan@cryptohippie.net>
2016-01-29 18:41:47 +0000

    Angular test script

---
+++ angularui/angularui-test.go
@@ -0,0 +1,71 @@
+// Provide test server for angular ui
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strconv"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("%s <port> <path>\n", os.Args[0])
+		os.Exit(1)
+	}
+	_, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("port: \"%s\" not a valid number\n", os.Args[1])
+		os.Exit(1)
+	}
+	datapath := os.Args[2]
+	muxer := http.NewServeMux()
+	muxer.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("===== Call begin =====")
+		fmt.Printf("%s %s\n", r.Method, r.URL)
+		fmt.Println("Headers:")
+		for k, v := range r.Header {
+			fmt.Printf("    %s: %s\n", k, v)
+		}
+		if r.Method == "GET" {
+			// Return file from path
+			urlParsed, _ := url.ParseRequestURI(r.RequestURI)
+			if urlParsed.Path == "" || urlParsed.Path == "/" {
+				urlParsed.Path = "index.html"
+			}
+			filename := path.Join(datapath, urlParsed.Path)
+			content, err := ioutil.ReadFile(filename)
+			if err != nil {
+				fmt.Printf("\"%s\": %s\n", filename, err)
+				w.Write([]byte("{ \"status\": \"error\" }"))
+			} else {
+				fmt.Printf("Serving: \"%s\"\n", filename)
+				w.Write(content)
+			}
+		} else {
+			// Print
+			if r.ContentLength > 0 {
+				body := make([]byte, r.ContentLength)
+				io.ReadFull(r.Body, body)
+				fmt.Printf("===== Body begin =====\n%s\n===== Body end =====\n", body)
+			}
+			w.Write([]byte("{ \"status\": \"ok\" }"))
+		}
+		fmt.Println("===== Call end =====\n\n\n")
+	})
+	s := &http.Server{
+		Addr:           ":" + os.Args[1],
+		Handler:        muxer,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
