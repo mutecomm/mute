3f6af7d7908cffbfe04c688e45e29b52f3b811d5
Frank Braun <frank@cryptogroup.net>
2016-01-31 12:04:24 +0000

    add missing copyright header

--- gui/angularui-test.go
+++ gui/angularui-test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 Mute Communications Ltd.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Provide test server for angular ui
 package main
 
