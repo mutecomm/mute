4101a4161517143cab19ce6645e652395ba38a34
Frank Braun <frank@cryptogroup.net>
2020-07-22 17:53:27 +0000

    change to Go modules

--- vendor/github.com/frankbraun/codechain/util/bzero/bzero.go
+++
@@ -1,9 +0,0 @@
-// Package bzero defines helper functions to zero sensitive memory.
-package bzero
-
-// Bytes sets all entries in the given byte slice buffer to zero.
-func Bytes(buffer []byte) {
-	for i := 0; i < len(buffer); i++ {
-		buffer[i] = 0
-	}
-}
